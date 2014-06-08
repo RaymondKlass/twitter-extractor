c6af8524623cfe3cb1639bb68e120d15e6038888
RaymondKlass <raymond@klassphoto.com>
2014-06-08 09:01:11 -0400

    cleaned up message parsing a bit

--- twitter_interface/message_parser.py
+++ twitter_interface/message_parser.py
@@ -1,60 +1,60 @@
-import re
+import re 
 
 
 class iterableException(Exception):
     pass
 
-# Basic parsing methods for twitter messages 
-# Accepts a twitter message in the form of a dictionary
+
+
 class message_parser(object):
+""" Message parser to aid in message classification tasks """
     
     def __init__(self, message):
-        
         self.message = message
-    
-    
+   
+        
     def regex_parser(self, field, expression):
-
+        """ Parses a message field based on a given regular expression """
+        
         try:
-            f = self.message[field]
+            return re.match(expression, self.message[field])
         except KeyError:
             return False
-
-        return re.match(expression, self.message[field])
-        
     
-    #  case sensitive is whether the search is case sensitive, require all is whether all words must be present - otherwise at least one is required
+    
+    
     def contains(self, field, search_list, case_sensitive = False, require_all = False):
+        """ 
+            Returns whether a message field contains any or all word from a search list 
+            Accepts a single field to test upon, and a list of search terms 
+        """
         
         try:
-            f = self.message[field]
+            search_field = self.message[field]
         except KeyError:
             return False
+
         
         if isinstance(search_list, basestring):
-             search_list = search_list.split(' ')
+             search_list = search_list.strip().split(' ')
         
         try:
             el = search_list[0]
-        except:
+        except KeyError:
             raise iterableException
         
+        
+        
         if not case_sensitive:
-            search_field = self.message[field].lower()
-        else:
-            search_field = self.message[field]
+            search_field = search_field.lower()
+            search_list = [word.lower() for word in search_list]
         
         
         for word in search_list:
-            if not case_sensitive:
-                test_word = word.lower()
-            else:
-                test_word = word
-                
-            if test_word not in search_field and require_all:
+            if word not in search_field and require_all:
                 return False
             
-            if test_word in search_field and not require_all:
+            if word in search_field and not require_all:
                 return True
         
         if require_all:
